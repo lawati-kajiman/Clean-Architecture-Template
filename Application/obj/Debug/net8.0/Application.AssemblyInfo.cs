f50482c6a86e3b0117e22e36618a32bd40f1472c
lawati-kajiman <k.abdulkhalam12e@gmail.com>
2024-11-17 11:42:45 +0545

    dotnet build

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Application layer for Clean Architecture template in .NET")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f33b0132bcc48d2c07a4969f0d66497cce8ef6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a37080dcbc13eaea9c1830578d3838a43ba6a2f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
